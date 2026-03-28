24298f4924d6efa9ea250bbc75843c8319ec2dc4
Aleksa Jojić <aleksajjc@gmail.com>
2026-03-28 18:02:49 +0100

    feat: add post(create) and index for Predavac

---
+++ backend/EventPlatform/EventPlatform/Controllers/PredavacController.cs
@@ -0,0 +1,55 @@
+﻿using EventPlatform.Data;
+using EventPlatform.Domen;
+using EventPlatform.Models.Dogadjaji;
+using EventPlatform.Models.Predavac;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventPlatform.Controllers
+{
+    public class PredavacController : Controller
+    {
+        public PredavacController(Context context)
+        {
+            this.context = context;
+        }
+        public Context context { get; }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new PredavacCreateViewModel());
+        }
+        [HttpPost]
+        public IActionResult Create(PredavacCreateViewModel model)
+        {
+            var noviPredavac = new Predavac
+            {
+                Ime = model.Ime,
+                Prezime = model.Prezime,
+                Titula = model.Titula,
+                OblastStrucnosti = model.OblastStrucnosti
+            };
+            context.Predavaci.Add(noviPredavac);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Index()
+        {
+            var predavaci = context.Predavaci.Select(p => new PredavacViewModel
+            {
+                PredavacID = p.PredavacID,
+                Ime = p.Ime,
+                Prezime = p.Prezime,
+                Titula = p.Titula,
+                OblastStrucnosti = p.OblastStrucnosti,
+                OdabraniStrucniDogadjaji = p.StrucniDogadjaji.Select(sd => new DogadjajViewModel
+                {
+                    Naziv = sd.Naziv,
+                    DatumVremeOdrzavanja = sd.DatumVremeOdrzavanja
+                }).ToList()
+            }).ToList();
+            return View(predavaci);
+        }
+    }
+}
---
+++ backend/EventPlatform/EventPlatform/Models/Predavac/PredavacCreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using EventPlatform.Domen;
+
+namespace EventPlatform.Models.Predavac
+{
+    public class PredavacCreateViewModel
+    {
+        public int PredavacID { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Titula { get; set; }
+        public string OblastStrucnosti { get; set; }
+        public List<int> StrucniDogadjajiIDs { get; set; }
+    }
+}
