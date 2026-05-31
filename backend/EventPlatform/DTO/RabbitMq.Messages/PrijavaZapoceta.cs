82bd8e1ac656a61b814b4e49de1bdb33f95bea12
Aleksa Jojić <aleksajjc@gmail.com>
2026-05-31 19:05:02 +0200

    feat: create RabbitMqBus with publish and subscribe methods and Dispatcher for outbox messages

--- backend/EventPlatform/DTO/RabbitMq.Messages/PrijavaZapoceta.cs
+++ backend/EventPlatform/DTO/RabbitMq.Messages/PrijavaZapoceta.cs
@@ -10,7 +10,7 @@ namespace DTO.Saga
     {
         public Guid CorrelationID { get; set; }
         public int StrucniDogadjajID { get; set; }
-        public int UcenikID { get; set; }
+        public int UcesnikID { get; set; }
         public double CenaKotizacije { get; set; }
         public string Email { get; set; }
     }
---
+++ backend/EventPlatform/Prijave.API/Services/Dispatcher.cs
@@ -0,0 +1,64 @@
+﻿
+using DTO.Saga;
+using Microsoft.EntityFrameworkCore;
+using Prijave.API.Data;
+using Prijave.API.Models;
+using System.Text.Json;
+
+namespace Prijave.API.Services
+{
+    public class Dispatcher(IServiceScopeFactory scopeFactory) : BackgroundService
+    {
+        private readonly IServiceScopeFactory _scopeFactory = scopeFactory;
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+
+                    using (var scope = _scopeFactory.CreateScope())
+                    {
+                        var dbContext = scope.ServiceProvider.GetRequiredService<PrijavaContext>();
+
+                        var outboxMessage = await dbContext.PrijavaZapocetaOutboxMessages
+                            .FirstOrDefaultAsync(x => x.Status == OutboxMessageStatus.ForProcessing, stoppingToken);
+                        if (outboxMessage != null)
+                        {
+                            var prijava = await dbContext.Prijave
+                                .Include(p => p.Ucesnik)
+                                .FirstOrDefaultAsync(x => x.CorrelationID == outboxMessage.CorrelationId, stoppingToken);
+                            if (prijava != null)
+                            {
+                                var prijavaZapoceta = new PrijavaZapoceta
+                                {
+                                    CorrelationID = prijava.CorrelationID,
+                                    StrucniDogadjajID = prijava.StrucniDogadjajID,
+                                    UcesnikID = prijava.UcesnikID,
+                                    Email = prijava.Ucesnik.Email,
+                                    CenaKotizacije = 0 
+                                };
+                                using var bus = new RabbitMqBus();
+                                await bus.Publish("prijava-zapoceta", JsonSerializer.Serialize(prijavaZapoceta));
+
+                                outboxMessage.Status = OutboxMessageStatus.Processed;
+
+                                dbContext.Update(outboxMessage);
+                                await dbContext.SaveChangesAsync(stoppingToken);
+
+                                Console.WriteLine($"[PRIJAVE-DISPATCHER] Poslata poruka prijava-zapoceta za CorrelationId: {prijava.CorrelationId}");
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"[PRIJAVE-DISPATCHER ERROR] {ex.Message}");
+                }
+                await Task.Delay(3000, stoppingToken);
+            }
+        }
+    }
+}
