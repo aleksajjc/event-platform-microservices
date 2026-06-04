5335575292db496bc2907f5faa103a3fce585e1d
Aleksa Jojić <aleksajjc@gmail.com>
2026-06-04 19:02:58 +0200

    feat: establish necessary ocelot packages(auth, cache..)

---
+++ backend/EventPlatform/EventPlatformGateway/Program.cs
@@ -0,0 +1,76 @@
+
+using EventPlatformGateway.Middlewares;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using System.Text;
+using Ocelot.Cache.CacheManager;
+namespace EventPlatformGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            builder.Configuration
+                    .SetBasePath(builder.Environment.ContentRootPath)
+                    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+                    .AddEnvironmentVariables();
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                            .AddJwtBearer(options =>
+                            {
+                                options.RequireHttpsMetadata = true;
+
+                                options.TokenValidationParameters = new TokenValidationParameters
+                                {
+                                    ValidateIssuer = true,
+                                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+
+                                    ValidateAudience = true,
+                                    ValidAudience = builder.Configuration["Jwt:Audience"],
+
+                                    ValidateIssuerSigningKey = true,
+                                    IssuerSigningKey = new SymmetricSecurityKey(
+                                        Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)),
+
+                                    ValidateLifetime = true,
+                                    ClockSkew = TimeSpan.Zero
+                                };
+                            });
+
+            // Ocelot + response cache.
+            builder.Services
+                .AddOcelot(builder.Configuration)
+                .AddCacheManager(x => x.WithDictionaryHandle());
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseMiddleware<RequestSecurityMiddleware>();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
